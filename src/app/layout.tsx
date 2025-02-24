bdef1f7390456866c290912a3285db483046bd04
Md Sohail Ansari <sohailworks10@gmail.com>
2025-02-24 20:41:54 +0530

    place code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const manrope = Manrope({
   variable: "--font-manrope",
@@ -22,6 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8916812543498250"
+        ></script>
+      </Head>
       <body className={`${manrope.variable} antialiased`}>
         {children}
         <Toaster />
